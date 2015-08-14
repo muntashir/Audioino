e4f87288cef9b8fe1806dd84f22904fb6c2f6274
Muntashir <kazimuntashir@gmail.com>
2015-08-14 04:44:16 -0400

    Initial commit

---
+++ Audioino.ino
@@ -0,0 +1,31 @@
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+
+const int analogInRight = A0;
+const int analogInLeft = A1;  
+const int analogOutRight = 6;
+const int analogOutLeft = 5;
+
+int audioIn = 0;     
+int audioOut = 0;    
+
+void setup() {  
+  //Set ADC prescale to 16 (77 kHz sampling)
+  cbi(ADCSRA, ADPS2);
+  sbi(ADCSRA, ADPS1);
+  sbi(ADCSRA, ADPS0);
+
+  //Set pins 5 and 6 to Fast PWM at 62.5 kHz
+  TCCR0B = 0;
+  sbi(TCCR0B, CS00);
+  sbi(TCCR0A, COM0A1);
+  sbi(TCCR0A, COM0B1); 
+  sbi(TCCR0A, WGM01); 
+  sbi(TCCR0A, WGM00); 
+}
+
+void loop() {
+  audioIn = (analogRead(analogInRight) + analogRead(analogInLeft)) / 2;
+  audioOut = map(audioIn, 0, 1023, 0, 255);
+  analogWrite(analogOutRight, audioOut);
+}
